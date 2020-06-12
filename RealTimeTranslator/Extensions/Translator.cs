b5300214dee1c002cea694f06202b62b6af6bec0
MRGRD56 <Bukinist56@mail.ru>
2020-06-13 00:24:56 +0500

    some changes

--- RealTimeTranslator/Extensions/Translator.cs
+++ RealTimeTranslator/Extensions/Translator.cs
@@ -7,7 +7,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
-using System.Windows;
 
 namespace RealTimeTranslator.Extensions
 {
@@ -15,55 +14,47 @@ namespace RealTimeTranslator.Extensions
 	{
 		public static string TranslateTextViaGoogle(string input, string lang2In, string lang2Out)
 		{
-			try
+			if (string.IsNullOrWhiteSpace(input))
 			{
-				if (string.IsNullOrWhiteSpace(input))
-				{
-					return input;
-				}
-				// Set the language from/to in the url (or pass it into this function)
-				string url = String.Format
-				("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
-				 lang2In, lang2Out, Uri.EscapeUriString(input));
-				HttpClient httpClient = new HttpClient();
-				string result = httpClient.GetStringAsync(url).Result;
-
-				// Get all json data
-				var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
-
-				// Extract just the first array element (This is the only data we are interested in)
-				var translationItems = jsonData[0];
-
-				// Translation Data
-				string translation = "";
-
-				// Loop through the collection extracting the translated objects
-				foreach (object item in translationItems)
-				{
-					// Convert the item array to IEnumerable
-					IEnumerable translationLineObject = item as IEnumerable;
-
-					// Convert the IEnumerable translationLineObject to a IEnumerator
-					IEnumerator translationLineString = translationLineObject.GetEnumerator();
-
-					// Get first object in IEnumerator
-					translationLineString.MoveNext();
-
-					// Save its value (translated text)
-					translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
-				}
-
-				// Remove first blank character
-				if (translation.Length > 1) { translation = translation.Substring(1); };
-
-				// Return translation
-				return translation;
+				return input;
 			}
-			catch (System.AggregateException e)
+			// Set the language from/to in the url (or pass it into this function)
+			string url = String.Format
+			("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+			 lang2In, lang2Out, Uri.EscapeUriString(input));
+			HttpClient httpClient = new HttpClient();
+			string result = httpClient.GetStringAsync(url).Result;
+
+			// Get all json data
+			var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+
+			// Extract just the first array element (This is the only data we are interested in)
+			var translationItems = jsonData[0];
+
+			// Translation Data
+			string translation = "";
+
+			// Loop through the collection extracting the translated objects
+			foreach (object item in translationItems)
 			{
-				MessageBox.Show($"{e.Message}\n{e.InnerException.Message}", "AggregateException", MessageBoxButton.OK, MessageBoxImage.Error);
-				return "*translation error*";
+				// Convert the item array to IEnumerable
+				IEnumerable translationLineObject = item as IEnumerable;
+
+				// Convert the IEnumerable translationLineObject to a IEnumerator
+				IEnumerator translationLineString = translationLineObject.GetEnumerator();
+
+				// Get first object in IEnumerator
+				translationLineString.MoveNext();
+
+				// Save its value (translated text)
+				translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
 			}
+
+			// Remove first blank character
+			if (translation.Length > 1) { translation = translation.Substring(1); };
+
+			// Return translation
+			return translation;
 		}
 
 		public static string ConvertLang3To2(string lang3) => lang3 switch
